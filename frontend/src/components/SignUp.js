a6b792cf2d1994eb20ce7de49be1613379ee0ba5
Haz-git <zhouharry1@gmail.com>
2020-10-14 11:29:28 -0700

    Created Form Submission

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -3,28 +3,38 @@ import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 
 
-const SignUp = () => {
+const SignUp = ({ handleSubmit }) => {
+
+    const handleOnSubmit = formValues => {
+        console.log(formValues);
+    }
+
+
+
     return (
         <div>
             <div>
                 <h1>Hello! Please sign up as a new user!</h1>
             </div>
-            <div>
-                <label>Please enter a name: </label>
-                <Field name='name' component='input'></Field>
-            </div>
-            <div>
-                <label>Please enter an email address: </label>
-                <Field name='name' component='input'></Field>
-            </div>
-            <div>
-                <label>Please enter a password: </label>
-                <Field name='name' component='input'></Field>
-            </div>
-            <div>
-                <label>Please confirm your password: </label>
-                <Field name='name' component='input'></Field>
-            </div>
+                <form onSubmit={handleSubmit(handleOnSubmit)}>
+                    <div>
+                        <label>Please enter a name: </label>
+                        <Field name='name' component='input'></Field>
+                    </div>
+                    <div>
+                        <label>Please enter an email address: </label>
+                        <Field name='email' component='input'></Field>
+                    </div>
+                    <div>
+                        <label>Please enter a password: </label>
+                        <Field name='password' component='input'></Field>
+                    </div>
+                    <div>
+                        <label>Please confirm your password: </label>
+                        <Field name='passwordConfirm' component='input'></Field>
+                    </div>
+                    <button type='submit'>Submit!</button>
+                </form>
             <div>
                 <Link to='/'>Go Back Home</Link>
             </div>
