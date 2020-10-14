10d07a090e15b36860984c5caf869df1a01dd4fd
Haz-git <zhouharry1@gmail.com>
2020-10-14 09:27:00 -0700

    Potential reducer for signup

---
+++ frontend/src/redux/userSignUp/userSignReducer.js
@@ -0,0 +1,11 @@
+import USER_SIGN_UP from './userSignTypes';
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+    if (action.type === USER_SIGN_UP) {
+        return {...state, user: action.payload}
+    } else {
+        return state;
+    }
+}
