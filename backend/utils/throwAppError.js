831cd5fb3c8cf74164d67fcf162d79239eccdfa5
Haz-git <zhouharry1@gmail.com>
2020-10-14 05:37:19 -0700

    Added Error Handling Class

---
+++ backend/utils/throwAppError.js
@@ -0,0 +1,12 @@
+//Custom Error handling class extending Error:
+
+class throwAppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+
+        this.status = `${statusCode}`.startsWith('4') ? 'Fail' : 'Error';
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
