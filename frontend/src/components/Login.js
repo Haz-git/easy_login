8bf2cb63d7cd9b869455926892d16b5480ce6242
Haz-git <zhouharry1@gmail.com>
2020-10-15 08:12:31 -0700

    Setting up Redux Forms for Login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 
 const Login = () => {
     return (
         <div>
-            <h2> This should be a login form requiring an email and a password </h2>
+            <div>
+                <h1>Hello! Welcome to the login page.</h1>
+                <h2>I hope you remember your email and password when you've signed up.</h2>
+            </div>
+            <div>
+                <div>
+                    <label>Please Enter Your Email Address:</label>
+                    <Field name='loginEmail' component='input' />
+                </div>
+                <div>
+                    <label>Please Enter Your Password</label>
+                    <Field name='loginPassword' component='input' />
+                </div>
+            </div>
             <Link to='/'>Go Back Home</Link>
         </div>
     )
 }
 
-export default Login;
+export default reduxForm({
+    form: 'loginForm'
+})(Login);
