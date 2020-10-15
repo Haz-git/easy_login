769edf7919b62aad1771e8eeb02262ad871e1fe0
Haz-git <zhouharry1@gmail.com>
2020-10-15 08:23:08 -0700

    Added Field Elements and OnSubmit

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,23 +3,31 @@ import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 
 
-const Login = () => {
+const Login = ({handleSubmit}) => {
+
+    const handleOnSubmit = formValues => {
+        console.log(formValues);
+    }
+
     return (
         <div>
             <div>
                 <h1>Hello! Welcome to the login page.</h1>
                 <h2>I hope you remember your email and password when you've signed up.</h2>
             </div>
-            <div>
+            <form onSubmit={handleSubmit(handleOnSubmit)}>
                 <div>
-                    <label>Please Enter Your Email Address:</label>
-                    <Field name='loginEmail' component='input' />
+                    <div>
+                        <label>Please Enter Your Email Address:</label>
+                        <Field name='loginEmail' component='input' />
+                    </div>
+                    <div>
+                        <label>Please Enter Your Password</label>
+                        <Field name='loginPassword' component='input' />
+                    </div>
                 </div>
-                <div>
-                    <label>Please Enter Your Password</label>
-                    <Field name='loginPassword' component='input' />
-                </div>
-            </div>
+                <button type='submit'>Submit</button>
+            </form>
             <Link to='/'>Go Back Home</Link>
         </div>
     )
